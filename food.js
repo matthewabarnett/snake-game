7cc759c5edaebad87a13ad9617aece13d7d9f39e
matthewabarnett <155782398+matthewabarnett@users.noreply.github.com>
2025-01-29 17:18:34 -0600

    created random grid postion for the food

--- food.js
+++ food.js
@@ -1,14 +1,28 @@
-let food = { x: 0, y: 0}
+import { onSnake, expandSnake } from './snake.js'
+import { randomGridPosition } from './grid.js'
+
+let food = getRandomFoodPosition()
+const EXPANSION_RATE = 1
 
 export function update () {
-   
+   if (onSnake(food)) {
+    expandSnake(EXPANSION_RATE)
+    food = getRandomFoodPosition()
+   }
 
 }
 export function draw(gameBoard) {
         const foodElement = document.createElement('div')
-        snakeElement.style.gridRowStart = food.y
-        snakeElement.style.gridColumnStart = food.x
-        snakeElement.classList.add('food')
+        foodElement.style.gridRowStart = food.y
+        foodElement.style.gridColumnStart = food.x
+        foodElement.classList.add('food')
         gameBoard.appendChild(foodElement)
     
+}
+function getRandomFoodPosition() {
+    let newFoodPosition
+    while(newFoodPosition == null || onSnake(newFoodPosition)) {
+        newFoodPosition = randomGridPosition()
+    }
+    return newFoodPosition
 }
